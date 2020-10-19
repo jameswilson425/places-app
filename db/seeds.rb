21522fa38e4e67ba76aea57fc08736a6fab4e95c
jimmy <jimewilson87@gmail.com>
2020-10-18 19:04:21 -0500

    creates backend

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: "The Second City", address: "1616 N Wells St, Chicago, IL 60614")
+Place.create(name: "The Annoyance", address: "851 W Belmont Ave, Chicago, IL 60657")
+Place.create(name: "The Crowd", address: "3935 North Broadway Ste 3039, Chicago, IL 60613")
+Place.create(name: "The Playground", address: "3209 N Halsted St, Chicago, IL 60657")
